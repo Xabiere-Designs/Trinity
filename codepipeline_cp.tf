5760eeff1cec75da7a172724320bc908952bf937
Xabiere-Designs <xabieredesigns@gmail.com>
2024-02-19 23:11:23 -0500

    Update codepipeline_cp.tf

--- codepipeline_cp.tf
+++ codepipeline_cp.tf
@@ -1,7 +1,7 @@
 resource "aws_codepipeline" "Pod2_HCN_Pipeline" {
 
   name     = var.project_name
-  role_arn = "arn:aws:iam::654654434704:role/CodePipelineServiceRole"
+  role_arn = "arn:aws:iam::#######:role/CodePipelineServiceRole"
 
 
   artifact_store {
